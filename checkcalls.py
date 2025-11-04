528f226fe881a67bad125faa42c151bfe41c82fb
Jagadeesh Malakannavar <njagadeesh@zeomega.com>
2025-11-04 17:22:17 +0530

    updated..

--- checkcalls.py
+++ checkcalls.py
@@ -1,3 +1,5 @@
+import requests
+
 calllists = []
 filecontents = []
 with open("calllist.txt", "r") as f:
@@ -10,3 +12,21 @@ with open("searchcall.html", "r") as f:
 for i in calllists:
     if not i in filecontents:
         print(f"<tr>{i}<td> </td><td> </td> <td> </td> <td> </td></tr>")
+
+
+url = "https://vu3jnm.co.in/searchcall.html"
+
+content = requests.get(url).text
+
+contents_list = content.split("\n")
+print("in web")
+for i in calllists:
+    if not i in content:
+        print(f"<tr>{i}<td> </td><td> </td> <td> </td> <td> </td></tr>")
+
+print("Find Dupplicates....")
+
+
+for i in calllists:
+    if content.count(i)>1:
+        print(f"{i} duplicated")
